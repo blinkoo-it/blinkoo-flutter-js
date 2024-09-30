1414418fec167627cd3e82b1f450a972b55361d9
Davide Quadrelli <davide.quadrelli@blinkoo.com>
2024-09-30 10:49:35 +0200

    feat: Added external parameters and api key

--- flutter_bootstrap.d.ts
+++ flutter_bootstrap.d.ts
@@ -1,1 +1,13 @@
-export declare const BlinkooFeed: any;
+export interface BlinkooFeedInterface {
+  init(): Promise<void>;
+  open(element: HTMLElement, params: BlinkooFeedParams): void;
+  close(): void;
+}
+
+export interface BlinkooFeedParams {
+  title?: string;
+  filters?: string;
+  playlistFilter?: string;
+  apiKey: string;
+}
+export declare const BlinkooFeed: BlinkooFeedInterface;
