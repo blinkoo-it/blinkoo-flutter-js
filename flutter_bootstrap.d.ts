dc72f7d656611763f09f77e2b9a87f08f74e623d
Davide Quadrelli <davide.quadrelli@blinkoo.com>
2024-10-01 11:43:17 +0200

    fix: Fixed typescript interface

--- flutter_bootstrap.d.ts
+++ flutter_bootstrap.d.ts
@@ -2,6 +2,8 @@ export interface BlinkooFeedInterface {
   init(): Promise<void>;
   open(element: HTMLElement, params: BlinkooFeedParams): void;
   close(): void;
+  _app?: any;
+  _viewId?: number;
 }
 
 export interface BlinkooFeedParams {
